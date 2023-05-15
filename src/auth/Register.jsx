40a692c0194d28df2a080a6f6c11f58ebe37b6f5
DAN <danigitari@gmail.com>
2023-05-15 19:38:04 +0300

    registrer

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -25,20 +25,20 @@ function Register() {
                 <div className="mb-5 flex justify-between gap-x-2 ">
                   <div>
                     <label className="block mb-1 text-sm font-semibold text-gray-900">
-                      FirstName
+                      First Name
                     </label>
                     <input
-                      placeholder="Name"
+                      placeholder="First name"
                       className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                       required
                     />
                   </div>
                   <div>
                     <label className="block mb-1 text-sm font-semibold text-gray-900">
-                      MiddleName
+                      Middle Name
                     </label>
                     <input
-                      placeholder="Name"
+                      placeholder="Last name"
                       className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                       required
                     />
@@ -46,7 +46,7 @@ function Register() {
                 </div>
                 <div className="mb-5">
                   <label className="block mb-1 text-sm font-semibold text-gray-900 ">
-                   LastName
+                   Last Name
                   </label>
                   <input
                     type="text"
