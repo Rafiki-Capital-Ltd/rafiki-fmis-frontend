91e534f5779e06098d89082c332cceb72b07b7e6
tallninja <ernestwambua2@gmail.com>
2023-09-25 22:29:16 +0300

    Switched to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import 'primereact/resources/primereact.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider, FarmProvider } from './context';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<AuthProvider>
 			<FarmProvider>
-				<BrowserRouter>
+				<HashRouter>
 					<App />
-				</BrowserRouter>
+				</HashRouter>
 			</FarmProvider>
 		</AuthProvider>
 	</React.StrictMode>
